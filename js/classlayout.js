897213ebb1b0d18fdda30be9e55c9ba5c828178d
blow18 <blow18@punahou.edu>
2016-10-03 09:24:59 -1000

    Created the javascript for painting the class layout.

--- js/classlayout.js
+++ js/classlayout.js
@@ -1,17 +1,45 @@
+var layout = [];
+
 window.onload = function () {
     var grid = document.getElementById("grid");
 
-    for (var i = 0; i < 6; i++) {
-        // Create the row
-        var row = document.createElement("div");
-        row.className = "row";
-        grid.appendChild(row);
+    for (var i = 0; i < 60; i++) {
+        var cell = document.createElement("div");
+        cell.className = "cell";
+        cell.gridIndex = i;
+        cell.addEventListener("click", onCellClick);
 
-        for (var j = 0; j < 10; j++) {
-            // Create cells
-            var cell = document.createElement("div");
-            cell.className = "cell";
-            row.appendChild(cell);
+        if (localStorage.layout) {
+            var existingLayout = JSON.parse(localStorage.layout);
+
+            if (existingLayout[i]) {
+                layout[i] = true;
+                cell.style.backgroundColor = "#8D99AE";
+            } else {
+                layout[i] = false;
+            }
+        } else {
+            layout[i] = false;
         }
+
+        grid.appendChild(cell);
     }
+
+    var next = document.getElementById("next");
+    next.addEventListener("click", function () {
+        localStorage.layout = JSON.stringify(layout);
+    });
 };
+
+function onCellClick(event) {
+    var element = event.currentTarget;
+    var index = element.gridIndex;
+
+    if (layout[index]) {
+        element.style.backgroundColor = "transparent";
+        layout[index] = false;
+    } else {
+        element.style.backgroundColor = "#8D99AE";
+        layout[index] = true;
+    }
+}
