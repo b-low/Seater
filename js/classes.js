d02daac141c6b369850993b7672f99781ff22dbd
blow18 <blow18@punahou.edu>
2017-01-23 14:53:23 -1000

    Added class deleting

--- js/classes.js
+++ js/classes.js
@@ -12,15 +12,14 @@ window.addEventListener("load", function() {
     var classContainer = document.getElementById("class-container");
     for (var i = 0; i < classes.length; i++) {
         var classroom = document.createElement("div");
-        classroom.className = "classroom button wrapper";
+        classroom.className = "classroom";
 
         var classroomTitle = document.createElement("h2");
         classroomTitle.innerText = "Class " + (i + 1);
-        classroomTitle.className = "classroom-title";
-
-        classroom.layout = classes[i].layout;
-        classroom.names = classes[i].names;
-        classroom.addEventListener("click", function() {
+        classroomTitle.className = "classroom-title button";
+        classroomTitle.layout = classes[i].layout;
+        classroomTitle.names = classes[i].names;
+        classroomTitle.addEventListener("click", function() {
             var element = event.currentTarget;
             localStorage.names = JSON.stringify(element.names);
             localStorage.layout = JSON.stringify(element.layout);
@@ -29,7 +28,23 @@ window.addEventListener("load", function() {
             document.location.href = "final.html";
         });
 
+        var deleteButton = document.createElement("p");
+        deleteButton.innerText = "X";
+        deleteButton.className = "delete button";
+        deleteButton.index = i;
+        deleteButton.addEventListener("click", function() {
+            if (confirm("Are you sure you want to delete this class?")) {
+                var element = event.currentTarget;
+
+                classes.splice(element.index, 1);
+                localStorage.classes = JSON.stringify(classes);
+
+                document.location.reload();
+            }
+        });
+
         classroom.appendChild(classroomTitle);
+        classroom.appendChild(deleteButton);
         classContainer.appendChild(classroom);
     }
 });
