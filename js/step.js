5d8feaca98e5495459bb8813542c0c00278130ea
blow18 <blow18@punahou.edu>
2017-02-25 23:53:48 -1000

    Added navigation.

--- js/step.js
+++ js/step.js
@@ -5,4 +5,13 @@ window.addEventListener("load", function() {
             window.location = "index.html";
         }
     });
+
+    var navLinks = document.getElementsByClassName("link");
+    for (var i = 0; i < navLinks.length; i++) {
+        navLinks[i].addEventListener("click", function() {
+            if (!confirm("You are currently leaving the class setup process and may lose all unsaved changes. Continue?")) {
+                e.preventDefault();
+            }
+        });
+    }
 });
